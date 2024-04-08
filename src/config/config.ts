bdc8d05933c4e29a8a947f1642a58ce5fa4450de
Rakesh K <codersgyan@gmail.com>
2024-04-08 14:04:15 +0300

    sign jwt token

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import userModel from "./userModel";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -24,9 +26,19 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Process
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Token generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
   // Response
-  res.json({ message: "User created" });
+  res.json({ accessToken: token });
 };
 
 export { createUser };
