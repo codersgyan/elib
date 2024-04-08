a1f9619a9cfe4b3e3a9bbb5381295320bd200696
Rakesh K <codersgyan@gmail.com>
2024-04-08 11:19:35 +0300

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to ebook apis.");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
+};
+
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+// Routes
+// Http methods: GET, POST, PUT, PATCH, DELETE
+app.get("/", (req, res, next) => {
+  res.json({ message: "Welcome to elib apis" });
+});
+
+export default app;
