fe6540edd7abc3d14748b286e74753426aff5745
Rakesh K <codersgyan@gmail.com>
2024-04-08 18:33:34 +0300

    update book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -64,4 +64,81 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createBook };
+const updateBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
+  const bookId = req.params.bookId;
+
+  const book = await bookModel.findOne({ _id: bookId });
+
+  if (!book) {
+    return next(createHttpError(404, "Book not found"));
+  }
+
+  // Check access
+  const _req = req as AuthRequest;
+  if (book.author.toString() !== _req.userId) {
+    return next(createHttpError(403, "You can not update others book."));
+  }
+
+  // check if image field is exists.
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  let completeCoverImage = "";
+  if (files.coverImage) {
+    const filename = files.coverImage[0].filename;
+    const converMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    // send files to cloudinary
+    const filePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads/" + filename
+    );
+    completeCoverImage = filename;
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: completeCoverImage,
+      folder: "book-covers",
+      format: converMimeType,
+    });
+
+    completeCoverImage = uploadResult.secure_url;
+    await fs.promises.unlink(filePath);
+  }
+
+  // check if file field is exists.
+  let completeFileName = "";
+  if (files.file) {
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads/" + files.file[0].filename
+    );
+
+    const bookFileName = files.file[0].filename;
+    completeFileName = bookFileName;
+
+    const uploadResultPdf = await cloudinary.uploader.upload(bookFilePath, {
+      resource_type: "raw",
+      filename_override: completeFileName,
+      folder: "book-covers",
+      format: "pdf",
+    });
+
+    completeFileName = uploadResultPdf.secure_url;
+    await fs.promises.unlink(bookFilePath);
+  }
+
+  const updatedBook = await bookModel.findOneAndUpdate(
+    {
+      _id: bookId,
+    },
+    {
+      title: title,
+      genre: genre,
+      coverImage: completeCoverImage ? completeCoverImage : book.coverImage,
+      file: completeFileName ? completeFileName : book.file,
+    },
+    { new: true }
+  );
+
+  res.json(updatedBook);
+};
+
+export { createBook, updateBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import express from "express";
-import { createBook } from "./bookController";
+import { createBook, updateBook } from "./bookController";
 import multer from "multer";
 import authenticate from "../middlewares/authenticate";
 
@@ -22,6 +22,16 @@ bookRouter.post(
     { name: "file", maxCount: 1 },
   ]),
   createBook
+);
+
+bookRouter.patch(
+  "/:bookId",
+  authenticate,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  updateBook
 );
 
 export default bookRouter;
